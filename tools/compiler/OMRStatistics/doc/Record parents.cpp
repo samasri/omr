f4ec85e5628d7d38bfc820b01239cd0f8f57e0c9
Samer AL Masri <almasri@ualberta.ca>
2018-01-18 13:24:09 -0700

    Found a way to read the name of the weird parent classes #15 + Updated doc with algorithm to record parents based on the new found parents

---
+++ tools/compiler/OMRStatistics/doc/Record parents.cpp
@@ -0,0 +1,21 @@
+recordParent(childClassName, parentClassName) {
+	if(parentClassName != childClassName) {
+		vector = map.find(childClassName)
+		if(!vector) create new vector
+		vector.add(parentClassName)
+	}
+}
+
+recordParents(classDecl) {
+	currentName = classDecl->getNameAsString();
+	BI = classDecl->bases_begin();
+	BE = classDecl->bases_end();
+	for(BC = BI; BC != BE; BC++) { //iterate through all bases
+		parent = BC->getType()
+		parentName = parent->getAsString()
+		if(shouldIgnoreClassName(parentName)) continue
+		recordParent(currentName, parentName)
+		parentDecl = parent->getCXXRecord()
+		if(parentDecl) recordParents(parentDecl)
+	}
+}
