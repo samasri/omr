a1111bccf43dd5c85dcf361854595aaa8a43d5a8
Samer AL Masri <almasri@ualberta.ca>
2018-01-26 15:09:34 -0700

    Fixed the output python files to take arguments and fix the files automatically

--- tools/compiler/OMRStatistics/output/remove.py
+++ tools/compiler/OMRStatistics/output/remove.py
@@ -1,13 +1,28 @@
-f = open('amd64.hierarchy','r')
-r = open('amd64.hierarchy.clean', 'w')
+import sys
+f1 = open(sys.argv[1] + '.hierarchy','r')
+r1 = open(sys.argv[1] + '.hierarchy.clean', 'w')
+f2 = open(sys.argv[1] + '.overloads','r')
+r2 = open(sys.argv[1] + '.overloads.clean', 'w')
+f3 = open(sys.argv[1] + '.overrides','r')
+r3 = open(sys.argv[1] + '.overrides.clean', 'w')
+
+def removeSTD(f, r):
+	while True:
+		x = f.readline()
+		x = x.rstrip()
+		if not ("std::" in x): 
+			list.append(x)
+		if not x: break
+
+	for line in list:
+		r.write(line + "\n")
+
 S = set()
 list = []
-while True:
-	x = f.readline()
-	x = x.rstrip()
-	if not ("std::" in x): 
-		list.append(x)
-	if not x: break
-
-for line in list:
-	r.write(line + "\n")
+removeSTD(f1, r1)
+S = set()
+list = []
+removeSTD(f2, r2)
+S = set()
+list = []
+removeSTD(f3, r3)
--- tools/compiler/OMRStatistics/output/trim.py
+++ tools/compiler/OMRStatistics/output/trim.py
@@ -1,13 +1,29 @@
-f = open('amd64.hierarchy','r')
-r = open('amd64.hierarchy.trimmed', 'w')
+import sys
+f1 = open(sys.argv[1] + '.hierarchy','r')
+r1 = open(sys.argv[1] + '.hierarchy.trimmed', 'w')
+f2 = open(sys.argv[1] + '.overloads','r')
+r2 = open(sys.argv[1] + '.overloads.trimmed', 'w')
+f3 = open(sys.argv[1] + '.overrides','r')
+r3 = open(sys.argv[1] + '.overrides.trimmed', 'w')
+
+
+def trim(f, r):
+	while True:
+		x = f.readline()
+		x = x.rstrip()
+		if not x: break
+		if not x in S: list.append(x)
+		S.add(x)
+
+	for line in list:
+		r.write(line + "\n")
+
 S = set()
 list = []
-while True:
-	x = f.readline()
-	x = x.rstrip()
-	if not x: break
-	if not x in S: list.append(x)
-	S.add(x)
-
-for line in list:
-	r.write(line + "\n")
+trim(f1, r1)
+S = set()
+list = []
+trim(f2, r2)
+S = set()
+list = []
+trim(f3, r3)
