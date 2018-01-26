7b56169e3285a23f735726fe95ae6f703ac602f2
Samer AL Masri <almasri@ualberta.ca>
2018-01-26 15:24:29 -0700

    Added a sanity check to the tests; made executing the tool on all files in amd64 a pre-requisit for tests

--- tools/compiler/OMRStatistics/tests/tests.py
+++ tools/compiler/OMRStatistics/tests/tests.py
@@ -1,7 +1,32 @@
+#Takes the path of the overrides output file (of all the classes) as an argument
 import random
 import sys
 import os
 import filecmp
+import csv
+
+def executeSanityCheck(input):
+	success = 1
+	for row in input:
+		if not row: continue
+		list = []
+		if row[0] == "TR":
+			className = row[1]
+			signature = row[2]
+			index = signature.find('(');
+			signatureName = signature[0:index];
+			if signatureName.lower() == className.lower():
+				list.append(row)
+		if len(list) > 0:
+			success = 0
+
+	if success: 
+		print("Sanity check succeeded")
+		return 1
+	else: 
+		print("Sanity check failed")
+		return 0
+
 
 inputDir = "tests/input"
 count = 0
@@ -18,5 +43,10 @@ for input in os.listdir(inputDir):
         hasEverFailed = 1
         testName = input.replace(".cpp","")
         print("Failed on Test: " + testName)
+
+overridesOutputFile = csv.reader(open(sys.argv[1],'r'), delimiter=";")
+if not executeSanityCheck(overridesOutputFile):
+	hasEverFailed = true
+
 if hasEverFailed: print("Other than that, all test cases succeeded")
 else: print("All test cases succeeded")
