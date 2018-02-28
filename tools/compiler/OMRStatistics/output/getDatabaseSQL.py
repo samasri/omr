be3a0f381dbc39eabd193f747a5245aa05d8a4d5
Samer AL Masri <almasri@ualberta.ca>
2018-02-28 15:46:01 -0700

    Created a python file to generate the queries in order to creand fill database #24

--- tools/compiler/OMRStatistics/output/getDatabaseSQL.py
+++ tools/compiler/OMRStatistics/output/getDatabaseSQL.py
@@ -88,7 +88,8 @@ def getOMRLongestPath():
 def insertTo(table, vector):
 	result = ''
 	result += 'INSERT INTO ' + table + ' VALUES('
-	for value in vector: result += str(value) + ', '
+	for value in vector: 
+		result += "'" + str(value) + "',"
 	result = result[:len(result)-2]
 	result += ')'
 	return result
@@ -99,7 +100,11 @@ nameIndex = path.index('getDatabaseSQL.py')
 path = path[:nameIndex]
 
 allFunctions = csv.reader(open(path + 'allFunctions','r'), delimiter=";")
+allFunctions2 = csv.reader(open(path + 'allFunctions','r'), delimiter=";") #Can iterate only once on one variable, need to iterate through this file twice
 allClasses = csv.reader(open(path + 'allClasses','r'), delimiter=";")
+overloads = csv.reader(open(path + '../visualization/Overloads/overloads','r'), delimiter=";")
+overrides = csv.reader(open(path + '../visualization/Overrides/overrides','r'), delimiter=";")
+hierarchies = csv.reader(open(path + '../visualization/Hierarchy/hierarchy','r'), delimiter=";")
 
 
 maxFunctionNameLength = -1
@@ -115,43 +120,95 @@ for row in allFunctions:
 # Create tables
 functions = functionTable(maxFunctionNameLength, maxSignatureLength)
 files = FileTable(getOMRLongestPath())
-overloads = OverloadTable()
-overrides = OverrideTable()
+overloadsTable = OverloadTable()
+overridesTable = OverrideTable()
 polymorphism = PolymorphismTable()
 classes = ClassTable(maxNamespaceLength, maxClassNameLength)
 
 # Print create table queries
 #print createTable(functions)
 #print createTable(files)
-#print createTable(overloads)
-#print createTable(overrides)
+#print createTable(overloadsTable)
+#print createTable(overridesTable)
 #print createTable(polymorphism)
 #print createTable(classes)
 
 # Fill tables
 classToIDMap = {}
+functionToIDMap = {}
+# Fill Class Table
 id = 1
 for row in allClasses:
 	#['ID', 'Namespace', 'ClassName', 'IsExtensible']
-	isExtensible = row[0]
-	classes = row[1].split(' --> ')
-	for fullClassName in classes:
-		lastColonIndex = fullClassName.rfind('::')
-		if lastColonIndex == -1: lastColonIndex = 0
-		namespace = fullClassName[:lastColonIndex]
-		className = fullClassName[lastColonIndex:]
-		classToIDMap[namespace + '::' + className] =  id
-		#print insertTo('Class', [id, namespace, className, isExtensible])
+	isExtensible = row[2]
+	namespace = row[0]
+	className = row[1]
+	qualifiedName = namespace + '::' + className if namespace != '' else className;
+	classToIDMap[qualifiedName] =  id #Fill classToIDMap from allClasses CSV File
+	print insertTo('Class', [id, namespace, className, isExtensible])
 	id += 1
 
-allFunctions = csv.reader(open(path + 'allFunctions','r'), delimiter=";")
-id = 1
-for row in allFunctions:
+# Fill Functions table
+id = 0
+for row in allFunctions2:
 	#['ID', 'FunctionName', 'Signature', 'ClassID', 'IsVirtual', 'IsImplicit', 'FileID']
-	if id == 1:
+	if id == 0:
 		id += 1
 		continue
-	classID = classToIDMap[row[3] + '::' + row[4]]
-	print classID
-	input = [id, row[0], row[1], 'ClassID', row[6], row[5], -1]
+	
+	qualifiedName = row[3] + '::' + row[4] if row[3] != '' else row[4]
+	classID = classToIDMap[qualifiedName]
+	functionToIDMap[row[1]] = id
+	print insertTo('Function', [id, row[0], row[1], classID, row[6], row[5], -1])
+	id += 1
+
+# Fill Overload table
+id = 0
+for row in overloads:
+	#self.columns = ['FunctionID', 'IsFirstOccurrence']
+	if id == 0: 
+		id += 1
+		continue
+	signature = row[1]
+	functionID = functionToIDMap[signature]
+	print insertTo('Overload', [functionID, row[2]])
+	
+# Fill Override table
+id = 0
+for row in overrides:
+	#['FunctionID', 'BaseClassID', 'OverridingClassID']
+	if id == 0: 
+		id += 1
+		continue
+	
+	signature = row[2]
+	functionID = functionToIDMap[signature]
+	
+	#Reconstruct qualfied names of classes & get IDs
+	qualifiedBaseName = row[0] + '::' + row[1] if row[0] != '' else row[1]
+	qualifiedOverridingName = row[3] + '::' + row[4] if row[3] != '' else row[4]
+	baseClassID = classToIDMap[qualifiedBaseName]
+	overridingClassID = classToIDMap[qualifiedOverridingName]
+	
+	print insertTo('Override', [functionID, baseClassID, overridingClassID])
+	
+# Fill Polymorphism table
+id = 0
+for row in hierarchies:
+	# ['RecordID', 'ChildClassID', 'ParentClassID']
+	if id == 0:
+		id += 1
+		continue
+	hierarchy = row[1]
+	previousClassID = ''
+	for clas in hierarchy.split(' --> '):
+		# Class Names in allClasses and hierarchy files are outputted using different methods, need to synch
+		if 'TR_' in clas and '::' not in clas: clas = clas.replace('TR_', 'TR::')
+		if 'TRPersistentMemoryAllocator' == clas: clas = 'TR::PersistentMemoryAllocator'
+		if previousClassID == '':
+			previousClassID = classToIDMap[clas]
+			continue
+		currentClassID = classToIDMap[clas]
+		print insertTo('Polymorphism', [id, previousClassID, currentClassID])
+		previousClassID = currentClassID
 	id += 1
