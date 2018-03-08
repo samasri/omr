97975ac358a946db017aab186abf4a5f160280b4
Samer AL Masri <almasri@ualberta.ca>
2018-03-08 02:27:09 -0700

    Updated makedot.py to calculate average hierarchy length

--- tools/compiler/OMRStatistics/visualization/Hierarchy/makeDot.py
+++ tools/compiler/OMRStatistics/visualization/Hierarchy/makeDot.py
@@ -1,13 +1,19 @@
-f = open('hierarchy', 'r')
-o = open('graph.dot', 'w')
+import sys
+
+path = sys.argv[0][:-10]
+
+f = open(path + 'hierarchy', 'r')
+o = open(path + 'graph.dot', 'w')
 
 o.write('digraph {\n')
 o.write('\tnode [shape=box];\n')
 o.write('\trankdir=BT;\n')
 o.write('\tedge[arrowhead="onormal"];\n')
 uniqueLines = set()
+length = []
 for row in f:
 	row = row.strip().split(" --> ")
+	length.append(len(row))
 	prevClas = -1
 	for clas in row:
 		if prevClas == -1: 
@@ -21,3 +27,11 @@ for row in f:
 		o.write(toWrite)
 		prevClas = clas
 o.write('}')
+
+average = 0;
+count = 0;
+for nb in length:
+	average += nb
+	count += 1
+average = float(average) / float(count)
+print 'Average hierarchy length: ' + str(float(average))
