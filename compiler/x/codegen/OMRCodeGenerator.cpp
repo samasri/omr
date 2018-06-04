8f790d9e01275e338cf7b1f7985c27b3fab1413d
Samer AL Masri <almasri@ualberta.ca>
2018-06-04 16:40:19 -0600

    Virtualized functions 5 #52

--- compiler/x/codegen/OMRCodeGenerator.cpp
+++ compiler/x/codegen/OMRCodeGenerator.cpp
@@ -1080,7 +1080,7 @@ OMR::X86::CodeGenerator::supportsMergingGuards()
    {
    return self()->getSupportsVirtualGuardNOPing() &&
           self()->comp()->performVirtualGuardNOPing() &&
-          self()->allowGuardMerging();
+          allowGuardMerging();
    }
 
 TR::RealRegister *
