e2a279cdf154df6389b0148518d67b4ffb85fbd3
Samer AL Masri <almasri@ualberta.ca>
2018-06-04 15:20:35 -0600

    Removed OMR_EXTENSIBLE from CodeGenerator #52

--- compiler/codegen/OMRCodeGenerator.hpp
+++ compiler/codegen/OMRCodeGenerator.hpp
@@ -247,7 +247,8 @@ TR::Node* generatePoisonNode(TR::Compilation *comp, TR::Block *currentBlock, TR:
 namespace OMR
 {
 
-class OMR_EXTENSIBLE CodeGenerator
+class /*OMR_EXTENSIBLE*/ CodeGenerator
+
    {
    private:
 
