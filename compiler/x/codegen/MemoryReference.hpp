f46557464d3af516d49afce82508a27bea5d6a1e
Samer AL Masri <almasri@ualberta.ca>
2018-11-08 02:52:07 -0700

    Commented out more OMR_EXTENSIBLE MemoryReferences
    Signed-off-by: Samer AL Masri <almasri@ualberta.ca>

--- compiler/x/codegen/MemoryReference.hpp
+++ compiler/x/codegen/MemoryReference.hpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2016 IBM Corp. and others
+ * Copyright (c) 2000, 2018 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -39,7 +39,7 @@ namespace TR { class SymbolReference; }
 namespace TR
 {
 
-class OMR_EXTENSIBLE MemoryReference : public OMR::MemoryReferenceConnector
+class /*OMR_EXTENSIBLE*/ MemoryReference : public OMR::MemoryReferenceConnector
    {
    public:
 
